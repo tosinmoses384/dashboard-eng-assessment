79d94e7a967c04857de4feb9e06f9882481d1ae8
Israel <tosinmoses384@gmail.com>
2022-11-18 13:09:01 +0100

    feat(layout):
      - create initial layout and folder structure.
      - install necessary dependencies
      - clean up code base and format with vs code.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import styled from "styled-components";
+import logo from "./logo.svg";
+import "./App.css";
+import { Button } from "antd";
+import { Navbar, Feed, SearchFeed } from "./components";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export const DivStyle = styled.div`
+  background-color: #000;
+`;
+
+const App = () => (
+  <BrowserRouter>
+    <DivStyle>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Feed />} />
+        <Route path="/search/:searchTerm" element={<SearchFeed />} />
+      </Routes>
+    </DivStyle>
+  </BrowserRouter>
+);
 
 export default App;
---
+++ src/components/index.tsx
@@ -0,0 +1,3 @@
+export { default as Navbar } from './Navbar';
+export { default as Feed } from './Feed';
+export { default as SearchFeed } from './SearchFeed';
